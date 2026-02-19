50146d12aa74adab67faf7f9f6cb5153096503f2
mnk <moti.nisenson@gmail.com>
2026-02-19 13:46:27 +0200

    Apply fix to ranges for what is visible

--- Loop/Managers/Live Activity/GlucoseActivityManager.swift
+++ Loop/Managers/Live Activity/GlucoseActivityManager.swift
@@ -157,10 +157,12 @@ class GlucoseActivityManager {
             
             var glucoseRanges: [GlucoseRangeValue] = []
             if let glucoseRangeSchedule = self.loopSettings.glucoseTargetRangeSchedule, let start = glucoseSamples.first?.startDate {
+                let historyInterval: TimeInterval = settings.addPredictiveLine ? .hours(-2) : .hours(-6)
+                let rangeStart = max(start, endDateChart.addingTimeInterval(historyInterval))
                 glucoseRanges = getGlucoseRanges(
                     glucoseRangeSchedule: glucoseRangeSchedule,
                     presetContext: presetContext,
-                    start: start,
+                    start: rangeStart,
                     end: endDateChart,
                     unit: unit
                 )
